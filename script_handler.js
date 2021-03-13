84c25dcdf9d9295cee72907d4549432907ed79b5
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-13 15:19:17 +0000

    commit

--- script_handler.js
+++ script_handler.js
@@ -5,13 +5,11 @@ function readFromScript(script_name)
 {
     document.title = script_name;
     var rawFile = new XMLHttpRequest();
-    rawFile.onreadystatechange = () => {
-        if (this.readyState == 4){
-            if (this.status == 200){
-                document.body.innerHTML = rawFile.responseText.replace(/(?:\r\n|\r|\n)/g, '<br>');
-            }else{
-                readFromScript("404Script");
-            }
+    rawFile.onloadend = () => {
+        if (rawFile.status == 200){
+            document.body.innerHTML = rawFile.responseText.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        }else{
+            readFromScript("404Script");
         }
     }
     rawFile.open("GET", `${window.location.origin}/scripts/${script_name}.lua`);
